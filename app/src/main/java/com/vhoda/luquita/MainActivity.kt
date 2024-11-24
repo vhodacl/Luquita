fe8f3a87e337896a33d26cf0fe7f641d735724fc
javi <javier@vhoda.cl>
2024-11-24 09:04:32 -0300

    update logo and mainactivity

--- app/src/main/java/com/vhoda/luquita/MainActivity.kt
+++ app/src/main/java/com/vhoda/luquita/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.vhoda.luquita
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.vhoda.luquita.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val SPLASH_DELAY = 3000L // 3 segundos
+    private val SPLASH_DELAY = 2000L // 3 segundos
 
     companion object {
         private const val TAG = "MainActivity"
@@ -21,10 +23,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
-        // Hacer la barra de estado y navegación transparente
-        window.decorView.systemUiVisibility = android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        // Configurar la visibilidad de los iconos de la barra de estado
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR  // Esto hace que los iconos sean oscuros
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+        
         window.statusBarColor = android.graphics.Color.TRANSPARENT
         window.navigationBarColor = android.graphics.Color.TRANSPARENT
 
