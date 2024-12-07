bebb9c897f85d7c7b0d7df9496963305e93656c7
javi <javier@vhoda.cl>
2024-12-07 08:21:46 -0300

    introduciendo sonido soggy

--- app/src/main/java/com/vhoda/luquita/EasterEggActivity.kt
+++ app/src/main/java/com/vhoda/luquita/EasterEggActivity.kt
@@ -1,18 +1,36 @@
 package com.vhoda.luquita
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vhoda.luquita.databinding.ActivityEasterEggBinding
 
 class EasterEggActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEasterEggBinding
+    private var mediaPlayer: MediaPlayer? = null // Variable para el MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEasterEggBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Configurar la imagen del gato
-        binding.imageView.setImageResource(R.drawable.soggy) // Asegúrate de tener una imagen llamada cat_image en res/drawable
+        binding.imageView.setImageResource(R.drawable.soggy)
+        // Configurar el click listener para la imagen
+        binding.imageView.setOnClickListener {
+            playSound() // Llamar a la función para reproducir el sonido
+        }
     }
-} 
+
+    private fun playSound() {
+        // Reproducir el sonido
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.sog) // Cargar el archivo de sonido
+        }
+        mediaPlayer?.start() // Iniciar la reproducción
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release() // Liberar recursos del MediaPlayer
+        mediaPlayer = null
+    }
+}
